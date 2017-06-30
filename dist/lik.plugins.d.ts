ab7f7230ef56a75832bfb80d713165fb969219ad
Phil Kunz <gitlab@philkunz.com>
2017-06-30 18:01:35 +0200

    fix rxjs wring typings

--- dist/lik.plugins.d.ts
+++ dist/lik.plugins.d.ts
@@ -4,3 +4,10 @@ export import events = require('events');
 export import lodash = require('lodash');
 export import minimatch = require('minimatch');
 export import rx = require('rxjs/Rx');
+import { Operator } from 'rxjs/Operator';
+import { Observable } from 'rxjs/Observable';
+declare module 'rxjs/Subject' {
+    interface Subject<T> {
+        lift<R>(operator: Operator<T, R>): Observable<R>;
+    }
+}
