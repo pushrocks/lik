011a865271d6a39841bfa5356c7d183af55341c3
Phil Kunz <gitlab@philkunz.com>
2017-07-05 14:37:26 +0200

    improve tests

--- test/test.looptracker.ts
+++ test/test.looptracker.ts
@@ -16,3 +16,12 @@ tap.test('should create a valid looptracker instance', async () => {
   myLoopTracker = new lik.LoopTracker()
   expect(myLoopTracker).to.be.instanceof(lik.LoopTracker)
 })
+
+tap.test('should add objects once and return true', async () => {
+  expect(myLoopTracker.checkAndTrack(object1)).to.be.true
+  expect(myLoopTracker.checkAndTrack(object1)).to.be.false
+  expect(myLoopTracker.checkAndTrack(object2)).to.be.true
+  expect(myLoopTracker.checkAndTrack(object2)).to.be.false
+})
+
+tap.start()
